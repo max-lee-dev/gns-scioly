37242681f3deffea6b95bcb97ae9882ac1b94e40
stqrin <maxplee8@gmail.com>
2023-05-28 21:47:38 -0400

    centered

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,17 +13,18 @@ export default function Home() {
               src="https://cdn.discordapp.com/attachments/691735762538070096/1112540429339070524/AJFCJaW7jDHLXa8jxVlh79-T7z5KuXD7yvjrZbwZ_eTo-iHnGgtsA7UlXcVT0JuL4jS5nFDM154SIvTHMTM4MSyxlfhtzwyC9h6jMYjBmg8oy9X82sYmnsvRSEpQY0HRhipR3IHtV4LQkNJ8tSD5gO8psfsSw1662-h937-s-no.png"
               alt=""
             ></img>
-            <Text
-              fontSize="64px"
-              fontWeight="500"
-              color="white"
-              position="absolute"
-              left="780"
-              top="200"
-              zIndex="1"
-            >
-              Great Neck South
-            </Text>
+            <Center>
+              <Text
+                fontSize="64px"
+                fontWeight="500"
+                color="#204a97"
+                position="absolute"
+                top="200"
+                zIndex="1"
+              >
+                Great Neck South
+              </Text>
+            </Center>
           </Box>
         </Center>
       </Box>
