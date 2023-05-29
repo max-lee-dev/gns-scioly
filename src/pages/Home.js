c4d28873663df382364c31a8982fff0bf484f70a
stqrin <maxplee8@gmail.com>
2023-05-28 21:39:01 -0400

    router odm added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,11 +7,16 @@ export default function Home() {
       <Box className="opacity">
         {" "}
         <Center>
-          <img
-            width="100%"
-            src="https://cdn.discordapp.com/attachments/691735762538070096/1112540429339070524/AJFCJaW7jDHLXa8jxVlh79-T7z5KuXD7yvjrZbwZ_eTo-iHnGgtsA7UlXcVT0JuL4jS5nFDM154SIvTHMTM4MSyxlfhtzwyC9h6jMYjBmg8oy9X82sYmnsvRSEpQY0HRhipR3IHtV4LQkNJ8tSD5gO8psfsSw1662-h937-s-no.png"
-            alt=""
-          ></img>
+          <Box>
+            <Text fontSize="24px" color="black" position="absolute">
+              h{" "}
+            </Text>
+            <img
+              width="100%"
+              src="https://cdn.discordapp.com/attachments/691735762538070096/1112540429339070524/AJFCJaW7jDHLXa8jxVlh79-T7z5KuXD7yvjrZbwZ_eTo-iHnGgtsA7UlXcVT0JuL4jS5nFDM154SIvTHMTM4MSyxlfhtzwyC9h6jMYjBmg8oy9X82sYmnsvRSEpQY0HRhipR3IHtV4LQkNJ8tSD5gO8psfsSw1662-h937-s-no.png"
+              alt=""
+            ></img>
+          </Box>
         </Center>
       </Box>
     </Box>
