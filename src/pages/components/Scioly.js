9f367c3e6b11ee2b24e9c9296deef2933dc2b01d
stqrin <maxplee8@gmail.com>
2023-06-03 17:17:11 -0400

    what is scioly

--- src/pages/components/Scioly.js
+++ src/pages/components/Scioly.js
@@ -6,6 +6,21 @@ function Scioly({ text }, ref) {
       <Box ref={ref} minH="70vh" paddingLeft="20px">
         <Box color="black" paddingTop="24px">
           <Text fontSize="34px">What is Science Olympiad?</Text>
+          <Box>
+            <Text paddingRight="5px" paddingTop="10px" fontSize="18px">
+              Science Olympiad is a science and technology club where students
+              test their skill and knowledge in 23 STEM events in specialties
+              such as engineering, coding, technology, cartography, earth
+              science, biology, forensics, physics, ornithology, astronomy, and
+              environmental science. For example, students prepare engineering
+              devices such as the Ping-Pong Parachute. In this event students
+              build a rocket that must launch into the air and then return an
+              egg back to the ground unbroken (using a student made parachute
+              system). Further students compete in academic and lab events, such
+              as Crime Busters where teams analyze forensics data and solve a
+              crime.
+            </Text>
+          </Box>
         </Box>
       </Box>
     </div>
